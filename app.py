e393714e9451e415f9c511f1061391f78c34751e
sai191202 <pellurusai22@gmail.com>
2025-07-05 22:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ import os
 
 app = Flask(__name__)
 
-# MongoDB connection setup
+
 client = MongoClient(os.getenv('mongodb+srv://pellurusai22:pellurusai22@cluster0.iwpdndh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 'mongodb://localhost:27017/'))
 db = client['webhook_db']
 actions_collection = db['actions']
+
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
 
-    
+    # Extract common details
     github_event = request.headers.get('X-GitHub-Event')
 
     if github_event == "push":
@@ -41,9 +43,15 @@ def webhook():
     else:
         return jsonify({"message": "Unsupported event type"}), 400
 
-    # Store in MongoDB
+    
     actions_collection.insert_one(doc)
     return jsonify({"message": "Webhook received"}), 200
+
+
+@app.route('/logs', methods=['GET'])
+def get_logs():
+    logs = list(actions_collection.find({}, {"_id": 0}))
+    return jsonify(logs)
 
 if __name__ == '__main__':
     app.run(port=5000)
